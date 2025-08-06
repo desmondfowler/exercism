a0e941c7f67d347d42184cf3216cbbb59b76e7e8
desmondfowler <desmondfowler14@gmail.com>
2025-08-06 16:02:43 -0500

    feat: lasagna problems

---
+++ go/lasagna/lasagna.go
@@ -0,0 +1,22 @@
+package lasagna
+
+// TODO: define the 'OvenTime' constant
+const OvenTime = 40
+
+// RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
+func RemainingOvenTime(actualMinutesInOven int) int {
+	var timeLeft = OvenTime - actualMinutesInOven
+	return timeLeft
+}
+
+// PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func PreparationTime(numberOfLayers int) int {
+	var prepTime = numberOfLayers * 2
+	return prepTime
+}
+
+// ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	var totalTime = PreparationTime(numberOfLayers) + actualMinutesInOven
+	return totalTime
+}
