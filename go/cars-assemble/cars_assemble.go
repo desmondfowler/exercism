39e71b2f34c6571d62fa09c12df818cc95911e0d
desmondfowler <desmondfowler14@gmail.com>
2025-08-07 12:02:46 -0500

    feat: solution for cars

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -18,8 +18,11 @@ func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int 
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	batchCost := (carsCount / 10) * 9500
-	spareCost := (carsCount % 10) * 10000
+	batchCost := (carsCount / 10) * 95000
+	spareCost := 0
+	if carsCount%10 != 0 {
+		spareCost = (carsCount % 10) * 10000
+	}
 	var totalCost uint = uint(batchCost + spareCost)
 	return totalCost
 }
