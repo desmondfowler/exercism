16cda2a5af8284a9d4ece815cfb665f39f2bb035
desmondfowler <desmondfowler14@gmail.com>
2025-08-08 07:10:22 -0500

    feat: a little math never hurts, good to know int division drops remainder

---
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,34 @@
+package purchase
+
+import "strings"
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	l_kind := strings.ToLower(kind)
+	if l_kind == "car" || l_kind == "truck" {
+		return true
+	}
+	return false
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	var firstLex string
+	if strings.Compare(option1, option2) < 0 {
+		firstLex = option1
+	} else {
+		firstLex = option2
+	}
+	return firstLex + " is clearly the better choice."
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age >= 10 {
+		return (originalPrice * 0.5)
+	} else if age >= 3 {
+		return (originalPrice * 0.7)
+	} else {
+		return (originalPrice * 0.8)
+	}
+}
